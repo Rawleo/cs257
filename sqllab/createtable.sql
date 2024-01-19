823d3bf926069801759034fdcfafab0f54837273
Rawleo <sonryan50@gmail.com>
2024-01-18 21:20:05 -0600

    sql data type playing

--- sqllab/createtable.sql
+++ sqllab/createtable.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS earthquakes;
 CREATE TABLE earthquakes (
   quakeDate text,
-  latitude text,
-  longitude text,
-  depth text,
-  magnitude text,
+  latitude double,
+  longitude double,
+  depth double,
+  magnitude double,
   magnitudeType text,
   id text,
-  locationSource text
+  locationSource char 
 );
