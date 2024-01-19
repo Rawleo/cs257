c9e60d60abc249a443ae8fd24c7a9f0103295f53
Rawleo <sonryan50@gmail.com>
2024-01-19 10:47:07 -0600

    sql query #3

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -14,6 +14,7 @@ ORDER by depth DESC;
 -- find earthquakes in Texas
 SELECT *
 FROM earthquakes
-WHERE longitude BETWEEN 26 and 36
-    AND latitude BETWEEN -107 and -93
-ORDER by magnitude DESC;
+WHERE locationSource = "tx";
+
+
+
