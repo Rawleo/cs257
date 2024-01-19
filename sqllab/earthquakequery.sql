dcacc1eacdaeb91aa9b270d1f64e160acdbf628e
Rawleo <sonryan50@gmail.com>
2024-01-19 10:40:45 -0600

    sql query #2

--- sqllab/earthquakequery.sql
+++ sqllab/earthquakequery.sql
@@ -2,10 +2,11 @@
 SELECT *
 FROM earthquakes
 WHERE depth>50 and depth <300
-ORDER BY depth ASC;
+ORDER BY depth ASC
+LIMIT 100;
 
 -- find earthquakes that are incredibly damaging
 SELECT *
 FROM earthquakes
 WHERE magnitude>7 and depth <300
-ORDER by depth DESC
+ORDER by depth DESC;
