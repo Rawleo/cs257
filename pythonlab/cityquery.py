a7b8f1c21276474e6fac2b917fbcb59cf8301e5d
Rawleo <sonryan50@gmail.com>
2024-01-25 13:01:06 -0600

    testing sql query

---
+++ pythonlab/cityquery.py
@@ -0,0 +1,63 @@
+import psycopg2
+
+def test_connection(): 
+
+	conn = psycopg2.connect(
+		host="localhost",
+		port=5432,
+		database="sonr",
+		user="sonr",
+		password="pies967beach"
+	)
+
+	if conn is not None:
+		print("Connection Worked!")
+	else:
+		print("Problem with Connection")
+	return None
+
+def connection_info():
+
+	conn_info = psycopg2.connect(
+                host="localhost",
+                port=5432,
+                database="sonr",
+                user="sonr",
+                password="pies967beach"
+	)
+	
+	return conn_info
+
+def checkExisting(itemName, columnName,tableName):
+
+	conn = connection_info()
+	cur = conn.cursor()
+	
+    sql = '''
+		SELECT CASE WHEN EXISTS (
+			SELECT %s 
+			FROM %s 
+			WHERE '%s' ~ %s
+		) 
+		THEN CAST(1 AS BIT) 
+		ELSE CAST(0 AS BIT) 
+		END;
+    ''', (columnName, tableName, itemName, columnName)
+
+	cur.execute(sql)
+	exists = cur.fetchone()
+
+	if exists:
+		return True
+	else:
+		return False
+
+
+def main():
+
+	test_connection()
+
+
+if __name__ == "__main__":
+
+    main()
