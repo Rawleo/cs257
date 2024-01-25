be74fba5146f8a4739dcc0ed6747330aafc45bc7
Rawleo <sonryan50@gmail.com>
2024-01-25 16:14:09 -0600

    Updating cityqueury.py

--- pythonlab/cityquery.py
+++ pythonlab/cityquery.py
@@ -175,20 +175,6 @@ def findStatePop():
 
 		if len(state) == 2:
 			state = state.upper()
-			sql   = '''
-				SELECT state, abb
-				FROM states t1
-				WHERE '%s' = t1.abb
-				
-			''' % (state)
-
-			cur.execute(sql)
-
-			state = cur.fetchone()[0]
-
-			if state == None:
-				continue
-
 		else:
 			state = state.capitalize()
 		
@@ -209,6 +195,7 @@ def findStatePop():
 
 		line       = cur.fetchone()
 		population = line[0]
+		state      = state
 
 		if line == None:
 			print("Please enter a valid state name or abbreviation. \n")
