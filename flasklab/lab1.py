800379453670db7889b3129f247cd7aad29adf5e
Rawleo <sonryan50@gmail.com>
2024-02-10 12:25:12 -0600

    Update lab1.py

--- flasklab/lab1.py
+++ flasklab/lab1.py
@@ -29,6 +29,63 @@ def connection_info():
 	
 	return conn_info
 
+# from cityquery.py in pythonlab
+def getStatePopulation(abbr):
+
+	conn = connection_info()
+	cur  = conn.cursor()
+
+	while True:
+		state = abbr
+
+		if len(state) == 2:
+			state = state.upper()
+		else:
+			state = state.capitalize()
+		
+		sql = '''
+
+			SELECT SUM(population)
+			FROM (
+				SELECT *
+				FROM states t1
+					JOIN uscitiestop1k t2 on t2.state = t1.state
+				WHERE '%s' = t1.abb
+					OR '%s' = t2.state
+			) as populationTable;
+
+		''' % (state, state)
+
+		cur.execute(sql)
+
+		line       = cur.fetchone()
+		population = line[0]
+		state      = state
+
+		if population == None:
+			print("Please enter a valid state name or abbreviation. \n")
+			continue
+			
+		else:
+			break
+
+	sql = '''
+		SELECT *
+		FROM states
+		WHERE '%s' = abb 
+			OR '%s' = state
+	''' % (state, state)
+
+	cur.execute(sql)
+
+	state  = cur.fetchone()[0]
+	string = state + ": " + str(population)
+
+	conn.commit()
+
+	return string
+
+
 def main():
 
 	app = flask.Flask(__name__)
@@ -40,7 +97,8 @@ def main():
 
 	@app.rount('/pop/<state_abbr>')
 	def statePopulation(abbr):
-		return None
+		population = getStatePopulation(abbr)
+		return str(population)
 	
 	my_port = 5133
 	app.run(host='0.0.0.0', port = my_port) 
