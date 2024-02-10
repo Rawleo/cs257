6ecd5286ecf0536c7256055ab0212afc5895858c
Rawleo <sonryan50@gmail.com>
2024-02-10 12:20:00 -0600

    Update lab1.py

--- flasklab/lab1.py
+++ flasklab/lab1.py
@@ -1,12 +1,50 @@
 import flask
+import psycopg2
 
-app = flask.Flask(__name__)
+def test_connection(): 
 
-@app.route('/add/<number1>/<number2>')
-def addAPI(number1, number2):
-    sum = int(number1) + int(number2)
-    return str(sum)
+	conn = psycopg2.connect(
+		host="localhost",
+		port=5432,
+		database="sonr",
+		user="sonr",
+		password="pies967beach"
+	)
+
+	if conn is not None:
+		print("\nConnection Worked!\n")
+	else:
+		print("\nProblem with Connection\n")
+	return None
+
+def connection_info():
+
+	conn_info = psycopg2.connect(
+                host="localhost",
+                port=5432,
+                database="sonr",
+                user="sonr",
+                password="pies967beach"
+	)
+	
+	return conn_info
+
+def main():
+
+	app = flask.Flask(__name__)
+
+	@app.route('/add/<number1>/<number2>')
+	def addAPI(number1, number2):
+		sum = int(number1) + int(number2)
+		return str(sum)
+
+	@app.rount('/pop/<state_abbr>')
+	def statePopulation(abbr):
+		return None
+	
+	my_port = 5133
+	app.run(host='0.0.0.0', port = my_port) 
 
 if __name__ == '__main__':
-    my_port = 5133
-    app.run(host='0.0.0.0', port = my_port) 
+
+	main()
