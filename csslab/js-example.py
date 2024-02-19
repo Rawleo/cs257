8c93ee2561f1f5018bdcab2ee84b628c4441f850
Rawleo <sonryan50@gmail.com>
2024-02-19 09:11:47 -0600

    update csslab

--- csslab/js-example.py
+++ csslab/js-example.py
@@ -4,10 +4,8 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
-    message = "Welcome to My Example Webpage."
-    message = message + " This text was produced by concatenating strings in Python!"
-    return render_template("homepage.html", someText = message)
+def welcome(): 
+    return render_template("homepage.html")
 
 if __name__ == '__main__':
     my_port = 5133
---
+++ csslab/static/functions.js
@@ -0,0 +1,45 @@
+
+
+the_heading = document.getElementById("hello");
+
+the_heading.onclick = function() {
+  the_heading.innerText = "Goodbye World!"
+};
+
+the_paragraph = document.getElementById("paragraph1");
+
+the_paragraph.onmouseenter = function() {
+  the_paragraph.style.color = "blue";
+  the_paragraph.style.backgroundColor = "white";
+};
+
+function changeColor() {
+  text_input_element = document.getElementById("user-color");
+  new_color = text_input_element.value;
+  the_heading.style.color = new_color;
+}
+
+function toggleColor() {
+  old_color = document.body.style.background;
+  if (old_color == 'white') {
+  	new_color = 'yellow';
+  } else {
+  	new_color = 'white';
+  }
+  document.body.style.background = new_color;
+}
+
+function randomSentence() {
+	name_input = document.getElementById("username");
+  paragraph2 = document.getElementById("paragraph2");
+  if (name_input.value != 0) {
+  	paragraph2.innerHTML = 'Hello, ' + name_input.value + '! Go to bed.';
+	} else {
+  	paragraph2.innerHTML = 'Please input a name.'
+  }
+}
+
+function randomNumber() {
+  button = document.getElementById("randomNumber");
+  button.innerHTML = Math.floor((Math.random() * 100000) + 1);
+}
